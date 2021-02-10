b209b42b8b818668400a0629d7c87be3dfd1da37
yusufabukar <yabukar64@gmail.com>
2021-02-10 23:02:59 +0000

    theming typography

---
+++ src/components/Text.jsx
@@ -0,0 +1,52 @@
+import styled from 'styled-components/native';
+
+const Text = styled.Text`
+	${({ theme }) => defaultTextStyles(theme)};
+	${({ type, theme }) => types[type](theme)}
+`;
+
+Text.defaultProps = {
+	type: 'body'
+};
+
+const defaultTextStyles = theme => `
+	flex-wrap: wrap;
+	margin-top: 0px;
+	margin-bottom: 0px;
+	font-weight: ${theme.fontWeights.regular};
+	font-family: ${theme.fonts.body};
+	color: ${theme.colours.text.primary}
+`;
+
+const body = theme => `
+	font-size: ${theme.fontSizes.body};
+`;
+
+const label = (theme) => `
+	font-size: ${theme.fontSizes.body};
+	font-weight: ${theme.fontWeights.medium};
+	font-family: ${theme.fonts.heading}
+`;
+
+const caption = theme => `
+	font-size: ${theme.fontSizes.caption};
+	font-weight: ${theme.fontWeights.bold}
+`;
+
+const hint = theme => `
+	font-size: ${theme.fontSizes.body}
+`;
+
+const error = theme => `
+	color: ${theme.colours.text.error}
+`;
+
+const types = {
+	body,
+	label,
+	caption,
+	hint,
+	error
+};
+
+export default Text;
