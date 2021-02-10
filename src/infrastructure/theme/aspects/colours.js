5eef7f7b463c528750881b3342aa3fcaad0a92d4
yusufabukar <yabukar64@gmail.com>
2021-02-10 21:31:24 +0000

    theme infrastructure

---
+++ src/infrastructure/theme/aspects/colours.js
@@ -0,0 +1,28 @@
+export const colours = {
+	brand: {
+		primary: '#2182BD',
+		secondary: '#5282BD',
+		muted: '#C6DAF7'
+	},
+	UI: {
+		primary: '#262626',
+		secondary: '#757575',
+		tertiary: '#F1F1F1',
+		quaternary: '#FFFFFF',
+		disabled: '#DEDEDE',
+		success: '#138000',
+		error: '#D0421B'
+	},
+	background: {
+		primary: '#FFFFFF',
+		secondary: '#F1F1F1'
+	},
+	text: {
+		primary: '#262626',
+		secondary: '#757575',
+		inverse: '#FFFFFF',
+		disabled: '#9C9C9C',
+		success: '#138000',
+		error: '#D0421B'
+	}
+};
---
+++ src/infrastructure/theme/aspects/fonts.js
@@ -0,0 +1,23 @@
+export const fonts = {
+	heading: 'Lato_400Regular',
+	body: 'Oswald_400Regular',
+	monospace: 'Oswald_400Regular'
+};
+
+export const fontSizes = {
+	title: '20px',
+	h1: '112px',
+	h2: '56px',
+	h3: '45px',
+	h4: '34px',
+	h5: '24px',
+	body: '16px',
+	button: '14px',
+	caption: '12px'
+};
+
+export const fontWeights = {
+	regular: 400,
+	medium: 500,
+	bold: 700
+};
