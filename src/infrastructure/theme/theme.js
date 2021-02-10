e659124811e6db3392ff260e0aecfce67104e48d
yusufabukar <yabukar64@gmail.com>
2021-02-10 16:55:13 +0000

    theme infrastructure

---
+++ src/infrastructure/theme/theme.js
@@ -0,0 +1,14 @@
+import { sizes } from './aspects/sizes.js';
+import { spacing, lineHeights } from './aspects/spacing.js';
+import { fonts, fontSizes, fontWeights } from './aspects/fonts.js';
+import { colours } from './aspects/colours.js';
+
+export const theme = {
+	sizes,
+	spacing,
+	lineHeights,
+	fonts,
+	fontSizes,
+	fontWeights,
+	colours
+};
